f764cd2a3e309b2129240a9d9250149cc3660d2e
cckdsjo2298e <85843855+cckdsjo2298e@users.noreply.github.com>
2026-04-09 11:56:20 -0400

    update faqs.ts to match old q and a pairs

--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -2,20 +2,41 @@ export const faqs = [
   {
     question: 'What is a hackathon?',
     answer:
-      'A hackathon is a free, weekend-long event where students learn new skills, build projects, and meet other people interested in tech.',
+      'A hackathon is a free, weekend-long tech event where students learn new skills, build interesting projects, and have fun! We will have free merch, free food, and industry professionals to connect with!',
   },
   {
     question: 'Do I need coding experience?',
     answer:
-      'Nope! Beginners are welcome, and design, product, and business skills are just as valuable as coding.',
+      'No coding experience is necessary! In fact, our best hacks have occasionally come from teams of non-CS people.',
+  },
+  {
+    question: 'Can I attend &hacks?',
+    answer:
+      'Come to &hacks! Work on a project (whether you have a team or not) to win prizes, or just come for the workshops and good vibes! No pressure; do what you feel. Please note for highschoolers that we are unable to chaperone minors.',
+  },
+  {
+    question: 'Are there rules?',
+    answer:
+      'You can use AI at &hacks! More material can be found in the Major League Hacking Code of Conduct.',
   },
   {
     question: 'Can I go solo?',
     answer:
-      'Yes! You can work solo or form a team before or during the event.',
+      'You can go solo or work in a team! Join our discord server to find a team: DISCORD, or attend the team-building workshop at &hacks itself.',
+  },
+  {
+    question: "What if I don't submit a project?",
+    answer:
+      "That's OK! You can still finish it on your own time after &hacks concludes.",
+  },
+  {
+    question: 'When can I start my project?',
+    answer:
+      'You can start your project when the schedule comes out! If you decide to continue a previous project, please make sure to be transparent about what you coded during &hacks and what portions were completed prior to the hackathon!',
   },
   {
     question: 'What if I have more questions?',
-    answer: "Reach out to us at computing@wm.edu, we're happy to help!",
+    answer:
+      'Please reach out to us with any questions or concerns at computing@wm.edu!',
   },
 ];
